9ece3e88b63767ef804206b4cc1075bb0b591739
parrt <parrt@cs.usfca.edu>
2017-07-31 13:57:02 -0700

    improve gradient descent

--- notes/code/descent.py
+++ notes/code/descent.py
@@ -5,22 +5,29 @@ import time
 
 
 def minimize(f, x0, eta, h, precision):
-    tracex = []
-    tracex.append(x0)
     x = x0
-    # print str(x0) + " x0"
     while True:
         prev = x
         finite_diff = f(x + h) - f(x)  # /h rolls into learning rate
         x = x - eta * finite_diff  # decelerates x step as it flattens out
-        # print "f(%1.12f) = %1.12f" % (x, f(x)),
-        tracex.append(x)
         delta = f(x) - f(prev)
-        # print ", delta = %1.20f" % delta
+        print "f(%1.12f) = %1.12f delta = %1.20f" % (x, f(x), delta)
         # stop when small change in vertical but not heading down
-        if delta >= 0 and abs(delta) < precision: break  # Sometimes subtraction wipes out precision and we get an actual 0.0
+        if delta >= 0 and abs(delta) < precision:
+            return x
     return tracex
 
+def minimize_trace(f, x0, eta, h, precision):
+    tracex = [x0]
+    x = x0
+    while True:
+        prev = x
+        finite_diff = f(x + h) - f(x)  # /h rolls into learning rate
+        x = x - eta * finite_diff  # decelerates x step as it flattens out
+        tracex.append(x)
+        delta = f(x) - f(prev)
+        if delta >= 0 and abs(delta) < precision:
+            return tracex
 
 if __name__ == '__main__':
     LEARNING_RATE = 10
@@ -38,7 +45,7 @@ if __name__ == '__main__':
     x0s = [random.uniform(.1, 1.2), random.uniform(.1, 1.2)]  # random starting positions
 
     print "TRIAL 1"
-    tracex = minimize(f, x0s[0], LEARNING_RATE, h, PRECISION)
+    tracex = minimize_trace(f, x0s[0], LEARNING_RATE, h, PRECISION)
     tracey = [f(x) for x in tracex]
     plt.rcParams.update({'font.size': 14})
     plt.plot(tracex, tracey, 'ro')
@@ -47,7 +54,7 @@ if __name__ == '__main__':
              color="red")
 
     print "TRIAL 2"
-    tracex = minimize(f, x0s[1], LEARNING_RATE, h, PRECISION)
+    tracex = minimize_trace(f, x0s[1], LEARNING_RATE, h, PRECISION)
     tracey = [f(x) for x in tracex]
     plt.rcParams.update({'font.size': 14})
     plt.plot(tracex, tracey, 'go')
@@ -56,5 +63,12 @@ if __name__ == '__main__':
              color="green")
 
     #plt.savefig('cos-trace-2minima.pdf', format="pdf")
+
+    ETA = 10
+    h = 0.0001
+    PRECISION = 0.0000001
+    x0 = random.uniform(.1, 1.2)
+    minx = minimize(f, x0, ETA, h, PRECISION)
+    print "f(%f) = %f" % (minx, f(minx))
 
     plt.show()

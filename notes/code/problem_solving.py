8b20f8bf00a5938386f571f888426c3a8a12b6b2
parrt <parrt@cs.usfca.edu>
2018-07-08 12:13:53 -0700

    update notes

--- notes/code/problem_solving.py
+++ notes/code/problem_solving.py
@@ -5,3 +5,22 @@ n = 0
 for d in s:
     n += int(d)
 print(n)
+
+
+# Reverse list
+
+A = [1, 9, 2, 4]
+"""
+example swap (this is a common pattern also):
+t = A[3]
+A[3] = A[0]
+A[0] = t
+"""
+print(A)
+n = len(A)
+for i in range(n//2): # 0..n/2-1 (stop halfway!)
+    t = A[n-i-1]
+    A[n-i-1] = A[i]
+    A[i] = t
+
+print(A)
